bdf0e3abb6f41233e1c75ee40d24df731684f4b8
unknown <michellenrisby@gmail.com>
2018-04-06 04:18:41 -0500

    personal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import Logo from './logo.png';
-import { auth, provider } from '../../firebase/firebase'
+import { auth, provider } from '../../firebase/firebase';
+import './Header.css';
 
 class Header extends Component {
   constructor() {
@@ -46,29 +47,43 @@ class Header extends Component {
   render() {
     
   return (
-      <nav>   
-        <img className='logo' src={Logo} alt='MK logo with light-bulb'/>
-        <h1>Inspiration</h1>
-        <div className='links'>
-            <Link to='/'> Home </Link>            
-            <Link to='/storycard'> StoryCard </Link>
-            <Link to='/admin'> Admin </Link> 
-          </div>
-         
+      <nav className='navbar navbar-default' role='navigation'> 
+        <div className='container'>
+          <div className='navbar-header'>
+            <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='#navbar-brand-centered'>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+            </button>
+            <div className='navbar-brand navbar-brand-centered'>
+              <img className='logo' src={Logo} alt='MK logo with light-bulb'/>
+              <h1>Inspiration</h1>
+            </div>
+            <div className='collapse navbar-collapse'  id='navbar-brand-centered'>
+              <ul className='nav navbar-nav'>
+                <li><Link to='/'> Home </Link></li>
+                <li><Link to='/storycard'> StoryCard </Link></li>
+                <li><Link to='/admin'> Admin </Link></li>
+              </ul>
+            </div>
+          </div>   
+        <div className='log-in'>        
         {this.state.user 
           ?
-          <button onClick={this.logout}>Log Out</button>
+          <button onClick={this.logout}> <i class="fa fa-user-circle" aria-hidden="true"> <br/>Log Out</i></button>
           :
-          <button onClick={this.login}>Log In</button>
+          <button onClick={this.login}>Log In <i class="fa fa-user-circle-o" aria-hidden="true"></i></button>
         }
         {this.state.user
           ?
-          <div > HELLO, {this.state.user.displayName.toUpperCase()}
+          <div > HELLO, {this.state.user.displayName.toUpperCase()} <i class="fa fa-user" aria-hidden="true"></i>
             </div>
           :
           <div> <p>You must be logged in to vote.</p>
             </div>
         }
+        </div>
+        </div>
       </nav>
     );
   }
--- src/components/StoryCard/StoryCard.js
+++ src/components/StoryCard/StoryCard.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import {Carousel} from 'react-bootstrap';
+import {Badge, Carousel} from 'react-bootstrap';
+import { Button, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
+import firebase from '../../firebase/firebase';
+import './StoryCard.css'
 
 class StoryCard extends Component {
   constructor(props) {
@@ -7,7 +10,12 @@ class StoryCard extends Component {
 
     this.state = {
       index: 0,
-      direction: null
+      direction: null,
+      cardTitle: '',
+      image: '',
+      siteURL: '',
+      about: '',
+      items: []
     };
     this.handleSelect = this.handleSelect.bind(this);
   }
@@ -18,40 +26,63 @@ class StoryCard extends Component {
       direction: e.direction
     });
   }
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title:items[item].title,
+          image: items[item].image,
+          site: items[item].site,
+          about: items[item].about
+        });
+      }
+      this.setState({
+        items: newState
+      });
+    });
+  }
 
   render() {
     const { index, direction } = this.state;
 
     return (
       <div>
-        <h1>StoryCards</h1> 
-      <Carousel
+        <h2>StoryCards</h2> 
+      <Carousel className='carousel'
         activeIndex={index}
         direction={direction}
         onSelect={this.handleSelect}>
-        <Carousel.Item>
-          <img width={900} height={500} alt='900x500' src='/carousel.png' />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width={900} height={500} alt='900x500' src='/carousel.png' />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width={900} height={500} alt="900x500" src="/carousel.png" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+          {this.state.items.map( (item) => {
+            return (
+              <Carousel.Item className='item'>
+                <Card className='card' reverse>
+                  <CardBody className='card-body'>
+                    <CardTitle className='card-title'>
+                      { item.title }
+                    </CardTitle>
+                    <CardText>
+                      { item.about }
+                    </CardText>
+                    <Button className='button'>
+                      <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                    </Button>
+                    <Badge>8</Badge>
+                    <Button className='button'>
+                      <i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+                    </Button>
+                    <Badge>5</Badge>
+                  </CardBody>
+                  <a className='img-card' href={ item.site } target='_blank'>
+                    <CardImage className="img-fluid" src={ item.image } alt='Image is not available'/>
+                  </a>
+                </Card> 
+            </Carousel.Item> 
+            )
+          })}
       </Carousel>
       </div>
     );
