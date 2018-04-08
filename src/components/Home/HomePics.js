dba6f97fb4bd135d8ab666a7bd5e1b9931f4a263
mrisby <35502928+mrisby@users.noreply.github.com>
2018-04-08 18:51:17 -0500

    personal

--- src/components/Home/HomePics.js
+++ src/components/Home/HomePics.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { CSSTransitionGroup } from 'react-addons-css-transition-group'
 import './Home.css'
-import HomePics from './HomePics'
 
 let btnActive = { 'background': '#06FF96' };
 let btnInactive = { 'background': '#FDD692' };
@@ -23,218 +22,18 @@ let transitionDivStyle = {
   'color': 'rgb(25,25,25)'
 };
 
-class Home extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      toggleAll: true,
-      fade: true,
-      slideIn: true,
-      slideUp: true,
-      slider: true,
-      height: true,
-      width: false,
-      size: false,
-      rotate: false
-    };
-    this.toggleAll = this.toggleAll.bind(this);
-    this.toggleFade = this.toggleFade.bind(this);
-    this.toggleSlideIn = this.toggleSlideIn.bind(this);
-    this.toggleHeightIn = this.toggleHeightIn.bind(this);
-    this.toggleSlideUp = this.toggleSlideUp.bind(this);
-    this.toggleSlider = this.toggleSlider.bind(this);
-    this.toggleWidthIn = this.toggleWidthIn.bind(this);
-    this.toggleSize = this.toggleSize.bind(this);
-    this.toggleRotate = this.toggleRotate.bind(this);
-  }
-  toggleAll() {
-    if (!this.state.toggleAll) {
-      this.setState({
-        toggleAll: true,
-        fade: true,
-        slideIn: true,
-        slideUp: true,
-        slider: true,
-        height: true
-      });
-    if (!this.state.width) {
-      this.toggleWidthIn();
-    }
-    if (!this.state.size) {
-      this.toggleSize();
-    }
-    if (!this.state.rotate) {
-      this.toggleRotate();
-    }
-    } else {
-      this.setState({
-        toggleAll: false,
-        fade: false,
-        slideIn: false,
-        slideUp: false,
-        slider: false,
-        height: false,
-        rotate: false
-      });
-      if (this.state.width) {
-        this.toggleWidthIn();
-      }
-      if (this.state.size) {
-        this.toggleSize();
-      }
-      if (this.state.rotate) {
-        this.toggleRotate();
-      }
-    }
-  }
-  toggleFade() {
-    if (!this.state.fade) {
-      this.setState({
-        fade: true
-      });
-    } else {
-      this.setState({
-        fade: false
-      });
-    }
-  }
-  toggleSlideIn() {
-    if (!this.state.slideIn) {
-      this.setState({
-        slideIn: true
-      });
-    } else {
-      this.setState({
-        slideIn: false
-      });
-    }
-  }
-  toggleSlideUp() {
-    if (!this.state.slideUp) {
-      this.setState({
-        slideUp: true
-      });
-    } else {
-      this.setState({
-        slideUp: false
-      });
-    }
-  }
-  toggleSlider() {
-    if (!this.state.slider) {
-      this.setState({
-        slider: true
-      });
-    } else {
-      this.setState({
-        slider: false
-      });
-    }
-  }
-  toggleHeightIn() {
-    if (!this.state.height) {
-      this.setState({
-        height: true
-      });
-    } else {
-      this.setState({
-        height: false
-      });
-    }
-  }
-  toggleWidthIn() {
-    if (!this.state.width) {
-      this.setState({
-        width: true
-      });
-      setTimeout( () => {
-        let element = document.getElementById('widthInId');
-        // element.classList.add('widthInTextAfter');
-      }, 15);
-    } else {
-      this.setState({
-        width: false
-      });
-      let element = document.getElementById('widthInId');
-      // element.className = element.className.replace('widthInTextAfter', '');
-    }
-  }
-  toggleSize() {
-    if (!this.state.size) {
-      this.setState({
-        size: true
-      });
-      setTimeout( () =>{
-        let element = document.getElementById('sizeDivId');
-        // element.classList.add('sizeTextAfter');
-      }, 15);
-    } else {
-      this.setState({
-        size: false
-      });
-      let element = document.getElementById('sizeDivId');
-      // element.className = element.className.replace('sizeTextAfter', ''); 
-    }
-  }
-  toggleRotate() {
-    if (!this.state.rotate) {
-      this.setState({
-        rotate: true
-      });
-      setTimeout( () => {
-        let element = document.getElementById('rotateDivId');
-        // element.classList.add('rotateTextAfter');
-      }, 15);
-    } else {
-      this.setState({
-        rotate: false
-      });
-      let element = document.getElementById('rotateDivId');
-      // element.className = element.className.replace('rotateTextAfter', '');
-    }
-  }
-  componentWillMount() {
-    this.toggleWidthIn();
-    this.toggleSize();
-    this.toggleRotate();
-  };
-
+class HomePics extends Component {
   render() {
-    return (
-      <div>
-        <div className='page-title'>
-          Hello World!
-        </div>
-          <Controller
-            toggleAllBtns = {this.state.toggleAll} />          
-        <div className = "transitionsContainer">
-          <FadeIn 
-            fade = {this.state.fade} toggleFade = {this.toggleFade} />
-          <SlideIn 
-            slideIn = {this.state.slideIn} toggleSlideIn = {this.toggleSlideIn} />
-          <SlideUp 
-            slideUp = {this.state.slideUp} toggleSlideUp = {this.toggleSlideUp} />
-          <Slider 
-            slider = {this.state.slider} toggleSlider = {this.toggleSlider} />
-          <HeightIn 
-            height = {this.state.height} toggleHeightIn = {this.toggleHeightIn} />
-          <WidthIn 
-            width = {this.state.width} toggleWidthIn = {this.toggleWidthIn} />
-          <SizeIn 
-            size = {this.state.size} toggleSize = {this.toggleSize} />
-          <RotateIn 
-            rotate = {this.state.rotate} toggleRotate = {this.toggleRotate} />
-        </div>
-      </div>
+    return(
+      <div></div>
     )
   }
 }
 
 class Controller extends Component {
   render() {
-    let toggleAllText = "Press Me To See Some Magic";
-    let toggleAllBtnStyle = btnInactive;
+    var toggleAllText = "Press Me To See Some Magic";
+    var toggleAllBtnStyle = btnInactive;
     if (this.props.toggleAllBtns) {
       toggleAllBtnStyle = btnActive;
       toggleAllText = "Press Me Again";
@@ -252,7 +51,7 @@ class Controller extends Component {
   }
 };
 
-class FadeIn extends Component {
+class FadeIn extends React.Component {
   render() {
     let image = {
       'image': 'https://cdn.pixabay.com/photo/2018/03/28/11/51/tattoo-3268988_1280.jpg'
@@ -275,7 +74,7 @@ class FadeIn extends Component {
   }
 };
 
-class SlideIn extends Component {
+class SlideIn extends React.Component {
   render() {
     let image = {
       'image': 'https://cdn.pixabay.com/photo/2018/01/31/07/36/sunset-3120484_1280.jpg'
@@ -298,7 +97,7 @@ class SlideIn extends Component {
   }
 };
 
-class SlideUp extends Component {
+class SlideUp extends React.Component {
   render() {
     let image = {
       'image': 'https://cdn.pixabay.com/photo/2018/02/04/21/13/monastery-3130879_1280.jpg'
@@ -321,7 +120,7 @@ class SlideUp extends Component {
   }
 };
 
-class Slider extends Component {
+class Slider extends React.Component {
   render() {
     let image = {
       'image': 'https://cdn.pixabay.com/photo/2018/01/31/09/42/people-3120717_1280.jpg'
@@ -344,7 +143,7 @@ class Slider extends Component {
   }
 };
 
-class HeightIn extends Component {
+class HeightIn extends React.Component {
   render() {
     let image = {
       'image': 'https://cdn.pixabay.com/photo/2016/07/29/02/20/wordpress-1550237_1280.jpg'
@@ -367,7 +166,7 @@ class HeightIn extends Component {
   }
 };
 
-class WidthIn extends Component {
+class WidthIn extends React.Component {
   render() {
     let image = {
       'image': 'https://cdn.pixabay.com/photo/2016/03/09/09/17/computer-1245714_1280.jpg'
@@ -391,7 +190,7 @@ class WidthIn extends Component {
   }
 };
 
-class SizeIn extends Component {
+class SizeIn extends React.Component {
   render() {
     let image = {
       'image': 'https://cdn.pixabay.com/photo/2017/08/25/06/45/urban-2679263_1280.jpg'
@@ -415,7 +214,7 @@ class SizeIn extends Component {
   }
 };
 
-class RotateIn extends Component {
+class RotateIn extends React.Component {
   render() {
     let image = {
       'image': 'https://cdn.pixabay.com/photo/2016/07/03/03/49/music-1494213_1280.jpg'
@@ -438,4 +237,4 @@ class RotateIn extends Component {
     );
   }
 };
-export default Home;
+export default HomePics;
